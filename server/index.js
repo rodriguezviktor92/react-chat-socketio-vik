991651b00533c4f2835cd5d377d088f83431cf25
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-08-21 18:17:22 -0500

    feat(package.json, server/config.js, server/index.js): config to deploy

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ import { Server as SocketServer } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
 import { PORT } from './config.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const server = http.createServer(app);
 new SocketServer(server);
 const io = new SocketServer(server, {
@@ -45,5 +49,6 @@ io.on('connection', (socket) => {
   });
 });
 
+app.use(express.static(join(__dirname, '../client/dist')));
 server.listen(PORT);
 console.log('Server started on port ', PORT);
